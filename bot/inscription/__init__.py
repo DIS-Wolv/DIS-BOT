07294a7ca2e5014e3229787af988e40a648b76a8
QuentinN42 <quentin@lieumont.fr>
2023-09-24 21:48:21 +0200

    fix: add inscription import
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

---
+++ bot/inscription/__init__.py
@@ -0,0 +1,16 @@
+"""Inscription module for the bot."""
+from bot.inscription.main import (
+    add,
+    addUser,
+    clear,
+    clearJoueur,
+    jourPage,
+    jourTransfo,
+    message,
+    missionName,
+    missionOrgaName,
+    orga,
+    remove,
+    remUser,
+    stateDLC,
+)
